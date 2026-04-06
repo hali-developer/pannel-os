45384a6645012a45e7a4ee4b8e72b0989b6f3ce7
Hammad Ali <hali35275@gmail.com>
2026-04-06 14:22:58 +0500

    db -config

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'super-secret-panel-key'
     # MySQL connection string defaults
     MYSQL_HOST = os.environ.get('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
-    PANEL_DB_NAME = os.environ.get('PANEL_DB_NAME', 'panel_db')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'pannel_user')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'StrongPass123!')
+    PANEL_DB_NAME = os.environ.get('PANEL_DB_NAME', 'pannel_db')
