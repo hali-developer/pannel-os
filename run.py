134e525150d8c204504c1f77cd23e3ac87d99aaa
Hammad Ali <hali35275@gmail.com>
2026-04-09 15:54:59 +0500

    fixed domain commands

--- run.py
+++ run.py
@@ -9,9 +9,9 @@ load_dotenv()
 
 from app import create_app
 
-config_name = os.environ.get('FLASK_ENV', 'production')
+config_name = os.environ.get('FLASK_ENV', 'development')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PANEL_PORT', 5246))
+    port = int(os.environ.get('PANEL_PORT', 8000))
     app.run(host='0.0.0.0', port=port, debug=app.config.get('DEBUG', False))
